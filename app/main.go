5b6ead54b50b66f13e0a0ab145c7052404dc53e0
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-07-31 20:53:16 +0530

    pass 1st stage

--- app/main.go
+++ app/main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
-	// "net"
+	"net"
 )
 
 func main() {
@@ -12,37 +11,42 @@ func main() {
 
 	// Uncomment this block to pass the first stage
 	//
-	// udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
-	// if err != nil {
-	// 	fmt.Println("Failed to resolve UDP address:", err)
-	// 	return
-	// }
-	//
-	// udpConn, err := net.ListenUDP("udp", udpAddr)
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to address:", err)
-	// 	return
-	// }
-	// defer udpConn.Close()
-	//
-	// buf := make([]byte, 512)
-	//
-	// for {
-	// 	size, source, err := udpConn.ReadFromUDP(buf)
-	// 	if err != nil {
-	// 		fmt.Println("Error receiving data:", err)
-	// 		break
-	// 	}
-	//
-	// 	receivedData := string(buf[:size])
-	// 	fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
-	//
-	// 	// Create an empty response
-	// 	response := []byte{}
-	//
-	// 	_, err = udpConn.WriteToUDP(response, source)
-	// 	if err != nil {
-	// 		fmt.Println("Failed to send response:", err)
-	// 	}
-	// }
+	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
+	if err != nil {
+		fmt.Println("Failed to resolve UDP address:", err)
+		return
+	}
+
+	udpConn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		fmt.Println("Failed to bind to address:", err)
+		return
+	}
+	defer func(udpConn *net.UDPConn) {
+		err := udpConn.Close()
+		if err != nil {
+			fmt.Println("Failed to close UDP connection:", err)
+		}
+	}(udpConn)
+
+	buf := make([]byte, 512)
+
+	for {
+		size, source, err := udpConn.ReadFromUDP(buf)
+		if err != nil {
+			fmt.Println("Error receiving data:", err)
+			break
+		}
+
+		receivedData := string(buf[:size])
+		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
+
+		// Create an empty response
+		var response []byte
+
+		_, err = udpConn.WriteToUDP(response, source)
+		if err != nil {
+			fmt.Println("Failed to send response:", err)
+		}
+	}
 }
